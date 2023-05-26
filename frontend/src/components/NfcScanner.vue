2d249ace44df48513f73296ed8c1ed0676e92e89
Vlad Kramarukha <vald1sl0ve.00@gmail.com>
2023-05-26 22:35:26 +0400

    dist

--- frontend/src/components/NfcScanner.vue
+++ frontend/src/components/NfcScanner.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 import nfc from '../services/nfc.ts'
+import { ref } from 'vue'
 
-function scan() {
-	nfc()?.scan().then((data) => {
-		console.log('DATA: ', data)
-	})
+const scanData = ref()
+
+async function scan() {
+	scanData.value = JSON.stringify(await nfc()?.scan())
 }
 </script>
 
 <template>
 	<div @click="scan" class="scanner"></div>
+	<div class="scanner-data">{{ scanData }}</div>
 </template>
 
 <style scoped>
@@ -19,5 +21,9 @@ function scan() {
 	background-color: crimson;
 	border-radius: 50%;
 	box-shadow: 5px 5px 2px 0 rgba(0, 0, 0, 0.15);
+}
+
+.scanner-data {
+	text-align: center;
 }
 </style>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
 	plugins: [vue()],
 	build: {
-		outDir: '../docs'
-	},
+		outDir: '../docs',
+		emptyOutDir: true
+	}
 })
